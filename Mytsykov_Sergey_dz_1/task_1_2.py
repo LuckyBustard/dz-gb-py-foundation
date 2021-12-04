0eb6b41e3d1c22b8fefa52bc06d8bf591c3c23bb
smytsykov <smytsykov@awg.ru>
2021-12-04 07:10:35 +0300

    ДЗ1 Отрефакторил - теперь число для добавления можно указать дополнительно

--- Mytsykov_Sergey_dz_1/task_1_2.py
+++ Mytsykov_Sergey_dz_1/task_1_2.py
@@ -4,22 +4,18 @@ for i in range(1, 1001):
     if i % 2 == 1:
         cube_odd_list.append(i ** 3)
 
-full_sum = [0, 0]
-term = 17
-for i in range(len(cube_odd_list)):
-    number = cube_odd_list[i]
-    digit_sum = 0
-    while number > 0:
-        digit_sum += number % 10
-        number //= 10
-    if digit_sum % 7 == 0:
-        full_sum[0] += cube_odd_list[i]
-    digit_sum = 0
-    number = cube_odd_list[i] + term
-    while number > 0:
-        digit_sum += number % 10
-        number //= 10
-    if digit_sum % 7 == 0:
-        full_sum[1] += cube_odd_list[i] + term
+terms = [0, 17]
+for term in terms:
+    full_sum = 0
 
-print(full_sum)
+    for i in range(len(cube_odd_list)):
+        number = cube_odd_list[i] + term
+        digit_sum = 0
+        while number > 0:
+            digit_sum += number % 10
+            number //= 10
+        if digit_sum % 7 == 0:
+            full_sum += cube_odd_list[i] + term
+
+    print('term:', term, 'sum:' , full_sum)
+
